869657cb2e4b764d7edc20bc2c97a68195ad728d
BlazeSN860 <virajprabhup@gmail.com>
2024-02-23 02:23:23 +0530

    events added

--- src/pages/events/data.ts
+++ src/pages/events/data.ts
@@ -1,82 +1,92 @@
 import in50hrs from "./assets/in50hrs.jpg";
+import amuse from "./assets/amuse.jpeg";
+import bridge from "./assets/bridge.jpeg";
+import inspiration from "./assets/inspiration.jpeg";
+import learning from "./assets/learning.jpeg";
+import salt from "./assets/salt.jpeg";
+import scale from "./assets/scale.jpeg";
+import scaleit from "./assets/scaleit.jpeg";
+import permute from "./assets/permute.png";
+
 
 export const CurrentEvents = [
   {
     id: 0,
-    img: in50hrs,
-    name: "ToCurrentEvents",
+    img: salt,
+    name: "Salt Mango Tree (SMT)",
     link: "https://gta.mulearn.org/",
     description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
+      "Every Wednesday, we conduct weekly sessions named Salt Mango Tree in the μLearn Discord voice channel, specifically in the \"Gathering Field.\" These sessions focus on enhancing spoken English skills, regardless of how well you speak. Additionally, they aim to improve conversational skills and alleviate social anxiety.",
   },
   {
     id: 1,
-    img: in50hrs,
-    name: "ToCurrentEvents",
+    img: inspiration,
+    name: "Inspiration Station Radio (ISR)",
     link: "https://gta.mulearn.org/",
     description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
+      "Every Tuesday, we conduct weekly sessions called Inspiration Station Radio held at the \"Gathering Field\" voice channel on the µLearn Discord Server. As the name suggests, this aims at inspiring young minds with the stories of people the same age or below or above, narrating their motivational journey and ensuring them that's it's possible to achieve great things regardless of your age.",
   },
   {
     id: 2,
-    img: in50hrs,
-    name: "ToCurrentEventss",
+    img: scale,
+    name: "Scale",
     link: "https://gta.mulearn.org/",
     description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
+      "Scale is a weekly event, hosted every Friday via Google Meet, where we showcase our internal talent pool. Experts from within our community share insights on a wide range of topics in which they specialize. This approach ensures that valuable knowledge from one person is effectively transferred to learners with similar interests.",
   },
+  {
+    id: 3,
+    img: scaleit,
+    name: "Scale It",
+    link: "https://gta.mulearn.org/",
+    description:
+      "It's similar to the Scale event. A weekly event, hosted every Friday via Google Meet. This event strives to bring talented tech enthusiasts out into the platform and provide an opportunity for them to teach those who seek knowledge.",
+  }
+
 ];
 
 export const UpComingEvents = [
   {
     id: 0,
-    img: in50hrs,
-    name: "TUpComingEvents",
+    img: bridge,
+    name: "Bridge The Gap",
     link: "https://gta.mulearn.org/",
     description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
+      "BRIDGE THE GAP is meant to be a connecting link between academia and industry standards, all for the sake of a technological student’s betterment. The panel discussion presided over by industrial professionals and eminent academics will strive to literally bridge the gap between these two symbiotic factors: a mind to think and hands to work.",
   },
   {
     id: 1,
     img: in50hrs,
-    name: "TUpComingEvents",
+    name: "In 50 Hours",
     link: "https://gta.mulearn.org/",
     description:
       "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
   },
   {
     id: 2,
-    img: in50hrs,
-    name: "TUpComingEvents",
+    img: permute,
+    name: "Permute",
     link: "https://gta.mulearn.org/",
-    description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
-  },
+    description: "PERMUTE is µLearn’s flagship event taking place on 23rd February at Park Centre, Phase 1, Technopark, Trivandrum, and is a testament to the constant evolution and rearrangement that µLearn undergoes. The next phase: µLearn 3.0 will be unveiled at the event, propelling our education into a new future."
+  }
 ];
 
 export const PreviousEvents = [
   {
     id: 0,
-    img: in50hrs,
-    name: "ToPreviousEvents",
+    img: amuse,
+    name: "Amuse",
     link: "https://gta.mulearn.org/",
     description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
+      "AMUSE was a dynamic interns' gathering fostering connections and networking. Being vibrant and energetic, it offered a platform for meaningful interactions and professional growth. This was an event where all the peers had a chance to know their µLearn family and dive deeper into the customs of it.",
   },
   {
     id: 1,
-    img: in50hrs,
-    name: "Top 0PreviousEvents",
+    img: learning,
+    name: "Learning Fest",
     link: "https://gta.mulearn.org/",
     description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
+      "Learning Fest, conducted in partnership with KKEM, presented a series of bootcamps aiming to enhance skills across diverse domains. Participants delve into workshops, earning karma points and unlocking numerous opportunities. This event initiated both personal growth and professional development, empowering individuals to excel in their respective interest areas.",
   },
-  {
-    id: 2,
-    img: in50hrs,
-    name: "ToPreviousEvents",
-    link: "https://gta.mulearn.org/",
-    description:
-      "Get ready to embark on a transformative journey of innovation and collaboration with the Grand Tech Adventures (GTA) series. Our 7-series offline hackathon saga is designed to redefine the way young minds experience technology and problem-solving. Each event is a chapter in this epic tech odyssey, uniting students, mentors, and industry leaders to explore a world of endless possibilities. Get ready for the next wave of innovation and tech brilliance as we introduce GTA: Sandshore Hackathon. After the success of CodeStorm, the adventure continues.",
-  },
+
 ];
