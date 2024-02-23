62ccf83c38c2a077fbbd36177914e878737cf78a
Amal C P <acp070146@gmail.com>
2024-02-23 07:41:41 +0530

    lc

--- src/pages/learning/components/LCDisplay.tsx
+++ src/pages/learning/components/LCDisplay.tsx
@@ -1,22 +1,26 @@
 import { useLocation } from "react-router-dom";
-import styles from './LCDisplay.module.css';
-type Props = {}
+import styles from "./LCDisplay.module.css";
+import { Topnav } from "../../../components/navbar/topnav";
+import { Nabvar } from "../../../components/navbar";
+type Props = {};
 
 export const LCDisplay = (_props: Props) => {
-
-    const location = useLocation();
-    const { Igs } = location.state;
-    const data = Igs.response.data
-    return (
-        <div>
-            {data.map((ig: any, index: number) => {
-                return (
-                    <div className={styles.ig} key={index}>
-                        <h2>{ig.name}</h2>
-
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+  const location = useLocation();
+  const { Igs } = location.state;
+  const data = Igs.response.data;
+  return (
+    <>
+      <Topnav />
+      <div className={styles.Wrapper}>
+        {data.map((ig: any, index: number) => {
+          return (
+            <div className={styles.ig} key={index}>
+              <h2>{ig.name}</h2>
+            </div>
+          );
+        })}
+      </div>
+      <Nabvar />
+    </>
+  );
+};
